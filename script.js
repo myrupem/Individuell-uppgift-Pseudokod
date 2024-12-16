d0c1f26b53782d8799903f823e85c216848843ec
myrupem <168198588+myrupem@users.noreply.github.com>
2024-12-16 14:20:05 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+// G-Version - Split the Nota
+// Skriv din pseudokod innanför nedanstående kommentarsblock
+/*
+
+
+*/
+
+// VG-Version - Lewis Carroll Word Puzzle
+// Skriv din pseudokod innanför nedanstående kommentarsblock
+// Startkoden är 100% frivillig och kan tas bort eller skrivas om
+/*
+
+play();
+
+function play()
+    SET variabel ordbok = [FOUR, FOUL, FOOL, FOOT, FORT, FORE, FIRE, FIVE]; // Innehåller ALLA ord i det engelska språket
+    SET variabel startOrd till "FOUR";
+    SET variabel slutordOrd till "FIVE";
+
+
+end function
+
+function isOneLetterApart(wordOne, wordTwo)
+    SET variabel diffCount till 0;
+
+    // Här behöver du skriva koden för din funktion
+
+    return diffCount === 1; // returnerar sant om endast en bokstav ändrats, annars falskt
+end function
+
+*/
